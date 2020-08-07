37b410ca3d169f7813e1eed6eeb2b8beb5b9e644
matias chattellino <matiaschattellino@gmail.com>
2020-08-07 11:41:09 -0300

    server funcionando

--- app.js
+++ app.js
@@ -1,48 +1,34 @@
 console.log("The bot is starting");
-//require Twit package, authenticate
 const express = require('express');
-//const routes = require('./routes');
 const Twit = require('twit');
 const config = require('./config');
 const T = new Twit(config);
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-//servidor
 const app = express();
 
-app.use(cors());
+const TOTAL_TWEETS = 5;
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
 
-// Set search query parameters 
-let params = {
-    q: 'obama',
-    count: 3
+function findTweets(term, callback) {
+    return T.get('search/tweets', {q: term, count: TOTAL_TWEETS}, (err, data, response) => {
+        // console.debug(err, data);
+        let tweets = [];
+        for (let tweet of data.statuses) {
+            tweets.push({"datetime": tweet.created_at, "text": tweet.text});
+        }
+        callback(tweets);
+    });
 }
 
-// Make request get(path: string, params, callback)
-T.get('search/tweets', params, gotData);
-
-
-//Handle the data returned by the request
-function gotData(err, data, response) {
-    let tweets = data.statuses;
-    for (let i=0; i < tweets.length; i++) {
-        console.log(tweets[i].text);
-    }
-};
-
 app.get("/", (request, response, next) => {
-    response.json([{"tweet": "asd"}]);
+    findTweets(request.query.search, (tweets) => { response.json(tweets); })
 });
 
 app.listen(5000, () => {
-    console.log("El servidor está inicializado en el puerto 3000");
+    console.log("El servidor está inicializado en el puerto 5000");
    });
-
-
-
-
-
