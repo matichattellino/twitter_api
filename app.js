a54c57bce59db465c7414a05c9ec5a170dcf21b3
matias chattellino <matiaschattellino@gmail.com>
2020-08-07 09:48:30 -0300

    first commit

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 console.log("The bot is starting");
-
 //require Twit package, authenticate
 const express = require('express');
-const routes = require('./routes');
+//const routes = require('./routes');
 const Twit = require('twit');
 const config = require('./config');
 const T = new Twit(config);
-
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
@@ -18,18 +16,15 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
 
-
-//puerto
-app.listen(5000);
-
 // Set search query parameters 
 let params = {
     q: 'obama',
-    count: 19
+    count: 3
 }
 
 // Make request get(path: string, params, callback)
 T.get('search/tweets', params, gotData);
+
 
 //Handle the data returned by the request
 function gotData(err, data, response) {
@@ -37,4 +32,17 @@ function gotData(err, data, response) {
     for (let i=0; i < tweets.length; i++) {
         console.log(tweets[i].text);
     }
-}
+};
+
+app.get("/", (request, response, next) => {
+    response.json([{"tweet": "asd"}]);
+});
+
+app.listen(5000, () => {
+    console.log("El servidor está inicializado en el puerto 3000");
+   });
+
+
+
+
+
