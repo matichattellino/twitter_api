eb5daf21d934f15643884a08233a9384c3fa81ec
matias chattellino <matiaschattellino@gmail.com>
2020-08-10 12:52:43 -0300

    set deploy

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: 'K0dF8k3kvN6yh1ybO4N1Wo7Gf',
+    consumer_secret: 'ImfbMthsoY71GQ5rTXnyu0IpqC4LuxAyY0xQ4hfs5GTjLAFKN0',
+    access_token: '139232743-RUPGLt4EKqv0F6v2c26pHUdkMQPKCRH0CwHyMKw5',
+    access_token_secret: '3YhckLdU7ZpKklm1JchCnjZzEKbWr5H9QkckDpiReYjam'
+}
